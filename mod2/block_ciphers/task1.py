40d53ea776e3a313e6b270ec9053910c6e5af682
JimmyChin946 <Judes946@gmail.com>
2025-09-30 10:52:25 -0700

    structure

--- mod2/block_ciphers/task1.py
+++ mod2/block_ciphers/task1.py
@@ -1,9 +1,11 @@
 import sys
 
+# from cbc import [your function here]
+# from ebc import [your function here]
+
 def task1():
     if len(sys.argv) == 1:
         plaintext_file = sys.argv[1]
-
     else:
         print("no command arguments provided")
 
