5e269d72f3f6312964daec0685757e2c1e6763fe
JimmyChin946 <Judes946@gmail.com>
2025-09-30 10:48:27 -0700

    start args for task 1

---
+++ mod2/block_ciphers/task1.py
@@ -0,0 +1,12 @@
+import sys
+
+def task1():
+    if len(sys.argv) == 1:
+        plaintext_file = sys.argv[1]
+
+    else:
+        print("no command arguments provided")
+
+
+if __name__ == "__main__":
+    task1()
