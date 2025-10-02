7646bdcb0bc5f4f0f5b493f7c5152873dbf0adaf
JimmyChin946 <Judes946@gmail.com>
2025-10-02 01:03:47 -0700

    functions take more single responsibility

--- mod2/block_ciphers/cbc/cbc.py
+++ mod2/block_ciphers/cbc/cbc.py
@@ -9,8 +9,8 @@ HEADER_SIZE: int = 54
 BLOCK_SIZE: int = 128 
 
 def implement_cbc(plaintext_file: str) -> None:
-    plaintext: bytes | None = read_bytes(plaintext_file)
-    if (plaintext == None):
+    text: bytes | None = read_bytes(plaintext_file)
+    if (text == None):
         print(f'error reading file: {plaintext_file}')
         return
 
@@ -22,7 +22,11 @@ def implement_cbc(plaintext_file: str) -> None:
     iv: bytes = random.randbytes(16)
     print(f'iv: {iv}')
     
-    encrypted_text: bytes = encrypt_cbc(plaintext, key, iv)
+    header: bytes = text[:HEADER_SIZE]
+    data: bytes = text[HEADER_SIZE:]
+    padded_data: bytes = add_padding(data, BLOCK_SIZE)
+
+    encrypted_text: bytes = header + encrypt_cbc(padded_data, key, iv)
 
     if (write_bytes('encrypted', encrypted_text) == None):
         print(f'error writing file')
@@ -37,18 +41,27 @@ def implement_cbc(plaintext_file: str) -> None:
         return
 
 def encrypt_cbc(text: bytes, key: bytes, iv: bytes) -> bytes:
-    header: bytes = text[:HEADER_SIZE]
-    data: bytes = text[HEADER_SIZE:]
-
-    padded_data: bytes = add_padding(data, BLOCK_SIZE)
-
     # Cipher Function
     cipher = AES.new(key=key, mode=AES.MODE_CBC, iv=iv)
 
     # encrypt BLOCK_SIZE at a time, each time adding it to the encrypted_text
-    encrypted_text: bytes = header
-    for i in range(int(len(padded_data)/BLOCK_SIZE)):
-        data_chunk: bytes = padded_data[(i*BLOCK_SIZE):((i+1)*BLOCK_SIZE)]
+    encrypted_text: bytes 
+    for i in range(int(len(text)/BLOCK_SIZE)):
+        data_chunk: bytes = text[(i*BLOCK_SIZE):((i+1)*BLOCK_SIZE)]
+        encrypted_chunk: bytes = cipher.encrypt(data_chunk)
+        encrypted_text = encrypted_text + encrypted_chunk
+
+    return encrypted_text 
+
+def decrypt_cbc(text: bytes, key: bytes, iv: bytes) -> bytes:
+    # Cipher Function
+    cipher = AES.new(key=key, mode=AES.MODE_CBC, iv=iv)
+
+    # encrypt BLOCK_SIZE at a time, each time adding it to the encrypted_text
+    decrypted_text: bytes 
+
+    for i in range(int(len(text)/BLOCK_SIZE)):
+        data_chunk: bytes = text[(i*BLOCK_SIZE):((i+1)*BLOCK_SIZE)]
         encrypted_chunk: bytes = cipher.encrypt(data_chunk)
         encrypted_text = encrypted_text + encrypted_chunk
 
