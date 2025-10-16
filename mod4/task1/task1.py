dea7863409ccb8472c4188f90adc9e4bb833240e
JimmyChin946 <Judes946@gmail.com>
2025-10-16 10:11:19 -0700

    task1 parta

--- mod4/task1/task1.py
+++ mod4/task1/task1.py
@@ -3,4 +3,10 @@ from dataclasses import dataclass
 from Crypto.Hash import SHA256
 import secrets
 
+def task1(input: bytes):
+    hash_obj = SHA256.new(input)
+    digest: bytes = hash_obj.digest()
+    print(f'[task1] digest: {digest.hex()}') 
+
 if __name__ == '__main__':
+    task1(b'Hello, World!')
