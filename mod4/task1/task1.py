bc934edc12a2f324bb281356142b6f030f6baba0
JimmyChin946 <Judes946@gmail.com>
2025-10-17 23:11:43 -0700

    about to beef it up

--- mod4/task1/task1.py
+++ mod4/task1/task1.py
@@ -1,24 +1,28 @@
-import sys
-from dataclasses import dataclass
+import random
+import time
+import os
+
 from Crypto.Hash import SHA256
-import secrets
-import random 
+from datetime import timedelta
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 # Hashes an arbitrary input and prints the digest to the screen in hexadecimal
 # format.
-def part_a(input: bytes) -> bytes:
-    print('Task1 Part A\n')
-
+def part_a(input: bytes, verbose: bool = True) -> bytes:
     hash_obj = SHA256.new(input)
     digest: bytes = hash_obj.digest()
-    print(f'[task1] digest: {digest.hex()}') 
+    if verbose: 
+        print(f'[task1] digest: {digest.hex()}') 
     
     return digest
 
 # Takes a string, turns it into bytes, gets a random string that has a hamming
 # distance of exactly 1 bit, and returns the SHA256 values of both of those
 def part_b(original: str) -> tuple[bytes, bytes]:
-    print('Task1 Part B\n')
 
     # Turn the string of interest into bytes
     original_bytes: bytes = original.encode('utf-8')
@@ -45,30 +49,117 @@ def get_single_hamm(input: bytes) -> bytes:
     byte_array[byte_index] ^= (1 << inner_bit_index)
     return(bytes(byte_array))
 
-#
-def part_c():
-    pass
+# Truncates a digest to a particular
+# to a particular domain (in bits)
+def part_c(digest: bytes, trunc_len: int) -> bytes:
+    # Bytes needed to remove from the digest 
+    # working with bytearrays are easier
+    truncate_bytes_count: int = trunc_len // 8
+    # Remaining fine tuning bits to "remove"
+    truncate_bits_count: int = trunc_len % 8
+
+    # Take the first n bytes
+    truncated_digest: bytearray = bytearray(digest[:truncate_bytes_count])
+
+    # Bitshift the remaining bits from the digest and tack it on the end of the
+    # truncated_digest
+    if truncate_bits_count > 0:
+        # Note that the remainder of the 'byte' is going to be just zeros
+        mask: float = (0xFF << (8-truncate_bits_count)) & 0xFF
+        last_byte: float = digest[truncate_bytes_count] & mask
+        truncated_digest.append(last_byte)
+
+    return bytes(truncated_digest)
+
+# 
+def process_graphs() -> None:
+    asset_path: str = './assets' 
+    dvc_path: str = os.path.join(asset_path, 'digest_sizes_v_collision_times.png')
+    dvi_path: str = os.path.join(asset_path, 'digest_sizes_v_imput_count.png') 
+
+    digest_sizes: list[int] = list(range(8, 51, 2))
+    collision_times: list[float] = [] # in seconds
+    input_count: list[int] = [] # in seconds
+
+    # =======================================================================
+
+
+    for b in digest_sizes:
+        start_time: float = time.perf_counter()
+        j: int = 0
+
+        while True:
+        # while j < 4:
+            j += 1
+
+            # Make random messages
+            m0_bytes: bytes = os.urandom(16) 
+            m1_bytes: bytes = os.urandom(16) 
+
+            # Get their digest
+            m0_digest: bytes = part_a(m0_bytes, False)
+            m1_digest: bytes = part_a(m1_bytes, False)
+
+            # Truncate their digest
+            m0_truncated_digest: bytes = part_c(m0_digest, b)
+            m1_truncated_digest: bytes = part_c(m1_digest, b)
+
+            # Check to see if they match
+            if m0_truncated_digest == m1_truncated_digest:
+                print(f'[{b}]message_0: {m0_bytes}')
+                print(f'[{b}]message_1: {m1_bytes}')
+                print(f'[{b}]Collision found at iteration {j} with digest: {m0_truncated_digest}')
+                print('-------------------------\n')
+                
+                elapsed_time: float = time.perf_counter() - start_time
+
+                collision_times.append(elapsed_time)
+                input_count.append(j)
+
+                break # break the while loop
+
+    print(f'digest_sizes: {digest_sizes}')
+    print(f'collision_times: {collision_times}')
+    print(f'input_count: {input_count}')
+
+    plt.plot(digest_sizes, collision_times)
+    plt.xlabel('Digest Sizes (bits)')
+    plt.ylabel('Collision Times(seconds)')
+    plt.title('Digest Sizes v Collision Times')
+
+    plt.savefig(dvc_path, bbox_inches='tight')
+    # plt.show()
+
+    plt.clf()
+
+    plt.plot(digest_sizes, input_count)
+    plt.xlabel('Digest Sizes (bits)')
+    plt.ylabel('Input (int)')
+    plt.title('Digest Sizes v Input Count')
+
+    plt.savefig(dvi_path, bbox_inches='tight')
+    # plt.show()
+
+    plt.clf()
 
 
 if __name__ == '__main__':
     # Part A
-    part_a(b'Hello, World!')
+    print('\n--- Task1 Part A ---\n')
+
+    foo_a: bytes = 'Hello, World!'.encode('utf-8')
+    part_a(foo_a)
 
     # Part B 
-    str1: str = 'Hello'
-    str2: str = 'beautiful'
-    str3: str = 'world!'
+    print('\n--- Task1 Part B ---\n')
 
+    str0: str = 'Hello'
+    str1: str = 'beautiful'
+    str2: str = 'world!'
+
+    hashed_str0_original, hashed_str0_hammed = part_b(str0)
     hashed_str1_original, hashed_str1_hammed = part_b(str1)
     hashed_str2_original, hashed_str2_hammed = part_b(str2)
-    hashed_str3_original, hashed_str3_hammed = part_b(str3)
 
-    # Part C
-
-
-    
-
-    
-
-
-
+    # Part C (option 1 because I am lazy...)
+    process_graphs()
